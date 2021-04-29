151fcc700d570d7f9a935cbeed2a0911dfbdf4c3
Clarence <clarence.e.a@gmail.com>
2021-04-29 21:40:55 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './images/logo.png';
+import armstrong from './images/armstrong-air.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1><img src={logo} alt="logo"/></h1>
       </header>
+      <div className="navbar">
+
+      </div>
+      <article>
+        <p>At Slowne heating and cooling we offer full system replacements. We do
+        not offer the following services:</p>
+        <ul>
+          <li>We don't do maintenances</li>
+          <li>We don't offer 24 hour service</li>
+          <li>We don't work weekends</li>
+          <li>We don't install anything other than Armstrong Air</li>
+          <img src={armstrong} id="armstrong-logo" alt="armstrong logo"/>
+          <li>We reserve the right to not do anything we don't want to</li>
+        </ul>
+        <p>To schedule a service call please call A-Aron</p>
+        <i>Or don't, whatever</i>
+      </article>
+      <footer>
+        <p>This website is still under construction</p>
+      </footer>
     </div>
-  );
+  )
 }
 
 export default App;
