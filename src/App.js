985114191a9e92819aae51eb6f38c18a7f1a277e
Clarence <clarence.e.a@gmail.com>
2021-05-15 18:12:20 +0000

    Updated testimonials to be a single item that allows you to scroll through available testimonials

--- src/App.js
+++ src/App.js
@@ -41,43 +41,6 @@ function App() {
     }
   )
 
-  // testimonial component props
-  const testimonials = [{
-    testimonial: "They were good AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-    + "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-    + "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-    customer: "Big Clarence"
-  },
-  {
-    testimonial: "Leave me alone",
-    customer: "Gauge Adams"
-  },
-  {
-    testimonial: "They're my favorite HVAC company",
-    customer: "Aaron Slone"
-  },
-  {
-    testimonial: "I like them as well",
-    customer: "Ajjea Slone"
-  },
-  {
-    testimonial: "An excellent company ran by an excellent baby boy",
-    customer: "Aaron's Mom"
-  },
-  {
-    testimonial: "The equipment they install is excellent quality",
-    customer: "Tim Jenks"
-  },
-  {
-    testimonial: "Southern Comfort's hoodies are better",
-    customer: "Anonymous Employee"
-  },
-  {
-    testimonial: "EeeEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEeeeeeeeEEEEEEEEEEEEEEEEEEe"
-    + "eeEeeeEEeeEEeeEeeEEeeEEeeEEeEEEEEEEEEEeeEeeeeeeEeeEeeEe",
-    customer: "Mini Me"
-  }]
-
   const services = [{
     serviceHeader: "Annual Maintenance Plans",
     serviceDescription: "Regular maintenance will increase the lifespan and "
@@ -134,12 +97,7 @@ function App() {
         <div id="testimonials" class="content-section">
           <h2>Testimonials <FontAwesomeIcon icon="comments"/></h2>
           <div className="line"/>
-          <div id="testimonials-content">
-            {testimonials.map((element) =>
-              <Testimonial testimonial={element.testimonial}
-              customer={element.customer}/>
-            )}
-          </div>
+          <Testimonials/>
         </div>
         <div id="services" class="content-section">
           <h2>Services <FontAwesomeIcon icon="tools"/></h2>
@@ -157,35 +115,8 @@ function App() {
           <div className="line"/>
           <p>To contact us, fill out the following form and A-Aron will visit
           you in the middle of the night when you least expect it!</p>
-          <div id="contact-form-wrapper">
-            <form id="contact-form" method="post">
-              <div id="form-group">
-                <div  className="contact-form-element">
-                  <label for="name"/>
-                  <input type="text" id="name" name="contact-name"
-                  placeholder="Name"/>
-                </div>
-                <div  className="contact-form-element">
-                  <label for="email"/>
-                  <input type="text" id="email" name="contact-email"
-                  placeholder="Email"/>
-                </div>
-                <div className="contact-form-element">
-                  <label for="address"/>
-                  <input type="text" id="address" name="contact-address"
-                  placeholder="Address"/>
-                </div>
-                <div className="contact-form-element">
-                  <label for="info"/>
-                  <textarea type="text" id="info" name="contact-info"
-                  placeholder="Message" style={{"resize": "none"}}/>
-                </div>
-                <div id="contact-form-button" className="contact-form-element"
-                placeholder="Info">
-                  <button type="submit">Contact</button>
-                </div>
-              </div>
-            </form>
+          <div>
+            <Contact/>
           </div>
           <i>This form does not actually do anything yet</i>
         </div>
@@ -197,13 +128,106 @@ function App() {
   )
 }
 
-function Testimonial(props) {
+function Testimonials() {
+
+  // testimonial component props
+  const testimonials = [{
+    testimonial: "They were good AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+    + "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+    + "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+    customer: "Big Clarence"
+  },
+  {
+    testimonial: "Leave me alone",
+    customer: "Gauge Adams"
+  },
+  {
+    testimonial: "They're my favorite HVAC company",
+    customer: "Aaron Slone"
+  },
+  {
+    testimonial: "I like them as well",
+    customer: "Ajjea Slone"
+  },
+  {
+    testimonial: "An excellent company ran by an excellent baby boy",
+    customer: "Aaron's Mom"
+  },
+  {
+    testimonial: "The equipment they install is excellent quality",
+    customer: "Tim Jenks"
+  },
+  {
+    testimonial: "Southern Comfort's hoodies are better",
+    customer: "Anonymous Employee"
+  },
+  {
+    testimonial: "EeeEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEeeeeeeeEEEEEEEEEEEEEEEEEEe"
+    + "eeEeeeEEeeEEeeEeeEEeeEEeeEEeEEEEEEEEEEeeEeeeeeeEeeEeeEe",
+    customer: "Mini Me"
+  }]
+
+  const [activeTestimonial, setActiveTestimonial] = useState(0)
+  const [showTestimonial, setShowTestimonial] = useState(true)
+
+  const fade = useSpring({
+    from: {opacity: 1},
+    to: {opacity: showTestimonial},
+    delay: 300
+  });
+
+  const previousTestimonial = () => {
+    if (activeTestimonial === 0) {
+
+    } else {
+      setShowTestimonial(0)
+      setTimeout(() => {setActiveTestimonial(activeTestimonial - 1)}, 750)
+      setTimeout(() => {setShowTestimonial(1)}, 750)
+    }
+  }
+
+  const nextTestimonial = () => {
+    if (activeTestimonial === testimonials.length - 1) {
+
+    } else {
+      setShowTestimonial(0)
+      setTimeout(() => {setActiveTestimonial(activeTestimonial + 1)}, 750)
+      setTimeout(() => {setShowTestimonial(1)}, 750)
+    }
+  }
+
+  return (
+    <div id="testimonials-content-wrapper">
+      <div id="testimonials-content">
+        <animated.div className="testimonial" style={fade}>
+          <div class="quote-left-wrapper">
+            <FontAwesomeIcon icon="quote-left" size="lg"/>
+          </div>
+          <p><i> {testimonials[activeTestimonial].testimonial} </i></p>
+          <div class="quote-right-wrapper">
+            <FontAwesomeIcon icon="quote-right" size="lg"/>
+          </div>
+          <div class="testimonial-line-wrapper">
+            <div class="testimonial-line"/>
+          </div>
+          <p>- {testimonials[activeTestimonial].customer}</p>
+        </animated.div>
+      </div>
+      <div id="testimonials-controls">
+        <button onClick={previousTestimonial}>Previous</button>
+        <button onClick={nextTestimonial}>Next</button>
+      </div>
+    </div>
+  )
+}
+
+function Service(props) {
   const [visible, setVisible] = useState(false)
 
   const fadeIn = useSpring({
     from: {opacity: 0},
     to: {opacity: visible ? 1 : 0},
-    delay: 400
+    delay: 300
   });
 
   const onChange = function (isVisible) {
@@ -214,28 +238,45 @@ function Testimonial(props) {
 
   return (
     <VisibilitySensor onChange={onChange} partialVisibility={true}>
-      <animated.div className="testimonial" style={fadeIn}>
-        <div class="quote-left-wrapper">
-          <FontAwesomeIcon icon="quote-left" size="lg"/>
-        </div>
-        <p><i> {props.testimonial} </i></p>
-        <div class="quote-right-wrapper">
-          <FontAwesomeIcon icon="quote-right" size="lg"/>
-        </div>
-        <div class="testimonial-line-wrapper">
-          <div class="testimonial-line"/>
-        </div>
-        <p>- {props.customer}</p>
+      <animated.div className="service" style={fadeIn}>
+        <h2>{props.serviceHeader}</h2>
+        <p>{props.serviceDescription}</p>
       </animated.div>
     </VisibilitySensor>
   )
 }
 
-function Service(props) {
+function Contact() {
   return (
-    <div className="service">
-      <h2>{props.serviceHeader}</h2>
-      <p>{props.serviceDescription}</p>
+    <div id="contact-form-wrapper">
+      <form id="contact-form" method="post">
+        <div id="form-group">
+          <div  className="contact-form-element">
+            <label for="name"/>
+            <input type="text" id="name" name="contact-name"
+            placeholder="Name"/>
+          </div>
+          <div  className="contact-form-element">
+            <label for="email"/>
+            <input type="text" id="email" name="contact-email"
+            placeholder="Email"/>
+          </div>
+          <div className="contact-form-element">
+            <label for="address"/>
+            <input type="text" id="address" name="contact-address"
+            placeholder="Address"/>
+          </div>
+          <div className="contact-form-element">
+            <label for="info"/>
+            <textarea type="text" id="info" name="contact-info"
+            placeholder="Message" style={{"resize": "none"}}/>
+          </div>
+          <div id="contact-form-button" className="contact-form-element"
+          placeholder="Info">
+            <button type="submit">Contact</button>
+          </div>
+        </div>
+      </form>
     </div>
   )
 }
